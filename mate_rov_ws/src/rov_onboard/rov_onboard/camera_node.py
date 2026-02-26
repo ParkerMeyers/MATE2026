02e9490e711fcd1147535876be2495c2c08240f8
Parker Meyers <parker@parkermeyers.com>
2026-02-26 18:59:23 -0500

    60fps

--- mate_rov_ws/src/rov_onboard/rov_onboard/camera_node.py
+++ mate_rov_ws/src/rov_onboard/rov_onboard/camera_node.py
@@ -18,7 +18,7 @@ class CameraNode(Node):
         self.declare_parameter('camera_index', 0)
         self.declare_parameter('frame_width', 640)
         self.declare_parameter('frame_height', 480)
-        self.declare_parameter('fps', 30)
+        self.declare_parameter('fps', 60)
         self.declare_parameter('publish_compressed', True)
         self.declare_parameter('jpeg_quality', 50)
 
